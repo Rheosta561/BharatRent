9d8a1abc98421b5ac8a2c166f6142023e0ba2373
Anubhav Mishra <manubhav731@gmail.com>
2025-03-16 19:08:03 +0530

    Done

--- Frontend/BharatRent/src/Screens/Home.jsx
+++ Frontend/BharatRent/src/Screens/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         <div className='flex flex-col items-center justify-center'>
             <h1 className='text-7xl font-bold'><span className='text-emerald-950'>Bharat</span>Rent</h1>
             <p className='text-md underline mt-2'>Your True Rental Companion </p>
-            <button className='text-sm bg-zinc-900 text-white p-4 mt-4 rounded-full' onClick={handleClic}>Get Started</button>
+            <button className='text-sm bg-zinc-900 text-white p-4 mt-4 rounded-full w-fit' onClick={handleClic}>Get Started</button>
         </div>
     </div>
   )
